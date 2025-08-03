9744a34955c0ea48d0674363c8de5d93a7d2440e
Anders <57843932+meanders1@users.noreply.github.com>
2025-08-03 19:50:13 +0200

    Split declaration and definition

--- fenz/array.hpp
+++ fenz/array.hpp
@@ -19,47 +19,27 @@ namespace fenz
         /// @brief Constructs a Iterable from a pointer to data.
         /// @param data Pointer to the data.
         /// @warning It is assumed that the data pointer points to an array of at least N elements.
-        Iterable(T *data) : data_(data) {}
+        Iterable(T *data);
 
         /// @brief Returns a reference to the element at the specified index.
         /// @tparam i Index of the element to access.
         /// @return Reference to the element at the specified index.
         template <int i>
-        T &at()
-        {
-            static_assert(i >= 0 && i < N, "Index out of bounds");
-            return data_[i];
-        }
+        T &at();
 
         /// @brief Returns a const reference to the element at the specified index.
         /// @tparam i Index of the element to access.
         /// @return Const reference to the element at the specified index.
         template <int i>
-        const T &at() const
-        {
-            static_assert(i >= 0 && i < N, "Index out of bounds");
-            return data_[i];
-        }
+        const T &at() const;
 
         /// @brief Performs an operation on each element of the array.
         /// @param func Function pointer to the operation to perform on each element. The parameters to the function are the element and its index.
-        void enumerate(void (*func)(T &, int))
-        {
-            for (int i = 0; i < N; ++i)
-            {
-                func(data_[i], i);
-            }
-        }
+        void enumerate(void (*func)(T &, int));
 
         /// @brief Performs a const operation on each element of the array.
         /// @param func Function pointer to the const operation to perform on each element. The parameters to the function are the element and its index.
-        void enumerate(void (*func)(const T &, int)) const
-        {
-            for (int i = 0; i < N; ++i)
-            {
-                func(data_[i], i);
-            }
-        }
+        void enumerate(void (*func)(const T &, int)) const;
 
         // Range-based for support
         T *begin() { return data_; }
@@ -72,15 +52,9 @@ namespace fenz
         /// @tparam Start Start index.
         /// @tparam End End index.
         /// @return An Iterable with the specified start index and end index.
-        /// @note This function does not create a copy.
+        /// @note This function does not create a copy of the data.
         template <int Start, int End>
-        Iterable<T, End - Start> view()
-        {
-            static_assert(Start >= 0 && End <= N, "Iterable out of bounds");
-            static_assert(End > Start, "Size must be positive");
-
-            return Iterable<T, End - Start>(data_ + Start);
-        }
+        Iterable<T, End - Start> view();
     };
 
     /// @brief A fixed-size array that owns its data.
@@ -95,22 +69,84 @@ namespace fenz
 
     public:
         /// @brief Default constructor.
-        Array() : Iterable<T, N>(ownedData_) {}
-
-        Array(const Array &) = delete;
-        Array &operator=(const Array &) = delete;
+        Array();
 
         /// @brief Constructs an Array with a default value for all elements.
         /// @param defaultValue The default value to initialize all elements of the array.
-        Array(T defaultValue) : Iterable<T, N>(ownedData_)
-        {
-            for (int i = 0; i < N; ++i)
-            {
-                ownedData_[i] = defaultValue;
-            }
-        }
+        Array(T defaultValue);
+
+        Array(const Array &) = delete;
+        Array &operator=(const Array &) = delete;
     };
 
+    // =====================================
+    // Implementations only below this point
+    // =====================================
+
+    template <typename T, int N>
+    inline Iterable<T, N>::Iterable(T *data) : data_(data)
+    {
+    }
+
+    template <typename T, int N>
+    template <int i>
+    inline T &Iterable<T, N>::at()
+    {
+        static_assert(i >= 0 && i < N, "Index out of bounds");
+        return data_[i];
+    }
+
+    template <typename T, int N>
+    template <int i>
+    inline const T &Iterable<T, N>::at() const
+    {
+        static_assert(i >= 0 && i < N, "Index out of bounds");
+        return data_[i];
+    }
+
+    template <typename T, int N>
+    inline void Iterable<T, N>::enumerate(void (*func)(T &, int))
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            func(data_[i], i);
+        }
+    }
+
+    template <typename T, int N>
+    inline void Iterable<T, N>::enumerate(void (*func)(const T &, int)) const
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            func(data_[i], i);
+        }
+    }
+
+    template <typename T, int N>
+    template <int Start, int End>
+    inline Iterable<T, End - Start> Iterable<T, N>::view()
+    {
+        static_assert(Start >= 0 && End <= N, "Iterable out of bounds");
+        static_assert(End > Start, "Size must be positive");
+
+        return Iterable<T, End - Start>(data_ + Start);
+    }
+
+    template <typename T, int N>
+    inline Array<T, N>::Array()
+        : Iterable<T, N>(ownedData_)
+    {
+    }
+
+    template <typename T, int N>
+    inline Array<T, N>::Array(T defaultValue)
+        : Iterable<T, N>(ownedData_)
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            ownedData_[i] = defaultValue;
+        }
+    }
 }
 
 #endif // FENZ_ARRAY_HPP
